eac6dc557da51743a882d23740b84ad6c537bcfb
anushanemaraj <153197729+anushanemaraj@users.noreply.github.com>
2025-05-19 22:03:06 +0530

    Add files via upload

---
+++ StringOperations/StringModificationDemo.java
@@ -0,0 +1,8 @@
+package adjlab3;
+public class StringModificationDemo {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String modified = str.replace("World", "Java");
+        System.out.println("Modified String: " + modified);
+    }
+}
