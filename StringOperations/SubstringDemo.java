760c29e9425b618988a75a5539a31dadce9e2aa0
anushanemaraj <153197729+anushanemaraj@users.noreply.github.com>
2025-05-19 22:00:18 +0530

    Add files via upload

---
+++ StringOperations/SubstringDemo.java
@@ -0,0 +1,8 @@
+package adjlab3;
+public class SubstringDemo {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Substring from index 5: " + str.substring(5));
+        System.out.println("Substring from index 5 to 11: " + str.substring(5, 11));
+    }
+}
