d15170b911d25c78a8564e6468632d34b7955c5c
anushanemaraj <153197729+anushanemaraj@users.noreply.github.com>
2025-05-19 22:05:55 +0530

    Add files via upload

---
+++ StringOperations/WhitespaceHandling.java
@@ -0,0 +1,8 @@
+package adjlab3;
+public class WhitespaceHandling {
+    public static void main(String[] args) {
+        String str = "   Java   ";
+        System.out.println("Original: '" + str + "'");
+        System.out.println("Trimmed: '" + str.trim() + "'");
+    }
+}
